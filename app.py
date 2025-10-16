b68b3fe174732d7919d4b555e0381773ade39832
BHUVANESH. A <abhuvanesh484@gmail.com>
2025-10-16 18:49:50 +0530

    Initialize Face Attendance System with Streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+st.title("Face Attendance System")
+
+# Example: just showing a message
+st.write("Your face attendance project will run here.")
+
+# If you have other modules, import them
+# from utils import some_function
